73fb33daea5f647e6deeabc76681c0d67b821586
Maxwell Seybert <maxwellseybert@gmail.com>
2022-09-06 10:37:57 -0600

    messed with react elements

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ root.render(
     <App />
   </React.StrictMode>
 );
+const element = <h1 className='testClass'>Hello World</h1>
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
